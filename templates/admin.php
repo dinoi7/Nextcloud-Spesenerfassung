8a816fbd1c648e7dee565cf17da3041ccdff3580
Reto Probst <dinoi@wff.ch>
2026-08-03 07:40:25 +0000

    F16: Dead admin settings code entfernt (AdminSettings.php, admin.php template, info.xml Settings-Block)

--- templates/admin.php
+++
@@ -1,7 +0,0 @@
-<div id="spesenerfassung-settings-admin"></div>
-<script nonce="<?php p(\OC::$server->getContentSecurityPolicyNonceManager()->getNonce()); ?>">
-document.addEventListener('DOMContentLoaded', function() {
-	// Admin settings are handled via the Vue SPA
-	// The API endpoints provide full settings management
-});
-</script>
